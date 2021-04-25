d192f1c9a9b087121dcbd28f8679111208fa390c
Jéssika <jessikafujimura@gmail.com>
2021-04-25 18:18:45 -0300

    Primeiro commit

--- src/com/br/dominio/Biblioteca.java
+++ src/com/br/dominio/Biblioteca.java
@@ -13,45 +13,33 @@ public class Biblioteca {
 
     public static void main(String[] args) {
         boolean validator = true;
-
         Random random = new Random();
-
         Scanner sc = new Scanner(System.in);
 
         System.out.println("**** CADASTRO DE USUÁRIO ****");
-
         System.out.println("Informe o nome do usuário: ");
         String nomeUsuario = sc.nextLine();
-
         System.out.println("Informe a data de nascimento do usuário: (ano-mes-dia) ");
         String dNascUsuario =  sc.nextLine();
-
         System.out.println("Informe o endereço: ");
         String endUsuario = sc.nextLine();
-
         Usuario user = new Usuario(
                 random.nextInt(100),
                 nomeUsuario,
                 LocalDate.parse(dNascUsuario),
                 endUsuario
         );
-
         user.imprimirUsuario();
 
         System.out.println("**** CADASTRO DE LIVRO ****");
-
         System.out.println("Informe o nome do livro: ");
         String nomeLivro = sc.nextLine();
-
         System.out.println("Informe o nome do autor do livro: ");
         String autorLivro =  sc.nextLine();
-
         Livro livro = new Livro(random.nextInt(1000), nomeLivro, autorLivro);
-
         livro.imprimirLivro();
 
         System.out.println("**** CADASTRO DE EMPRESTIMO ****");
-
         while (validator){
             System.out.println("Informe a data de solicitação: (ano-mes-dia)");
             String dataSolicitacao = sc.nextLine();
